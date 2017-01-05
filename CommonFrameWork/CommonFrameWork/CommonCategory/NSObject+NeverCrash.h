01015a103f8e7f3c9b3e02be2e8523c072197f46
luocheng <maowangxin_2013@163.com>
2017-01-06 00:30:36 +0800

    利用消息转发机制，做永不崩溃处理

---
+++ CommonFrameWork/CommonFrameWork/CommonCategory/NSObject+NeverCrash.h
@@ -0,0 +1,16 @@
+//
+//  NSObject+NeverCrash.h
+//  CommonFrameWork
+//
+//  Created by Luke on 2017/1/6.
+//  Copyright © 2017年 okdeer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  利用消息转发机制，做永不崩溃处理
+ */
+@interface NSObject (NeverCrash)
+
+@end
