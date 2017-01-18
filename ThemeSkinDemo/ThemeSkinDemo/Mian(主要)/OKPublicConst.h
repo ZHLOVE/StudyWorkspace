e9439aaed5099610af9e69280db87f99cc2fdca8
luocheng <maowangxin_2013@163.com>
2017-01-18 12:53:06 +0800

    整理文件

--- ThemeSkinDemo/ThemeSkinDemo/Mian(主要)/OKPublicConst.h
+++ ThemeSkinDemo/ThemeSkinDemo/Mian(主要)/OKPublicConst.h
@@ -3,13 +3,13 @@
 #import <UIKit/UIKit.h>
 
 /** tabBar item标题key */
-UIKIT_EXTERN NSString * const CCTabBarItemTitleKey;
+UIKIT_EXTERN NSString * const OKTabBarItemTitleKey;
 
 /** tabBar背景图片名字key */
-UIKIT_EXTERN NSString * const CCTabBarBgImageKey;
+UIKIT_EXTERN NSString * const OKTabBarBgImageKey;
 
 /** tabBar普通状态图片名字key */
-UIKIT_EXTERN NSString * const CCTabBarNormolImageKey;
+UIKIT_EXTERN NSString * const OKTabBarNormolImageKey;
 
 /** tabBar被选中状态图片名字key */
-UIKIT_EXTERN NSString * const CCTabBarSelectedImageKey;
+UIKIT_EXTERN NSString * const OKTabBarSelectedImageKey;
