c4a1c21b8fe05661f90429bedb325c2834016293
maowangxin <maowx@okdeer.com>
2017-07-19 17:49:06 +0800

    修改侧滑App分类

--- CommonFrameWork/CommonFrameWork/CommonCategory/UITabBarController+OKSliderApp.m
+++ CommonFrameWork/CommonFrameWork/CommonCategory/UITabBarController+OKSliderApp.m
@@ -9,6 +9,9 @@
 #import "UITabBarController+OKSliderApp.h"
 #import <objc/runtime.h>
 
+//左侧控制器的起始位置
+#define kLeftVCStartX            (-80)
+//左侧控制器最大的便宜位置
 #define MaxOffsetX               (self.view.frame.size.width-80)
 //获取屏幕宽度
 #ifndef Screen_Width
@@ -22,6 +25,7 @@
 static char const * const kLeftMaskView      = "kLeftMaskView";
 static char const * const kRightMaskView     = "kRightMaskView";
 static char const * const kTabBarMaskView    = "kTabBarMaskView";
+static char const * const kLeftViewController = "kLeftViewController";
 static char const * const KHasOpen           = "KHasOpen";
 
 @interface UITabBarController ()<UIGestureRecognizerDelegate>
@@ -31,12 +35,26 @@ static char const * const KHasOpen           = "KHasOpen";
 @property (nonatomic, strong) UIView *rightMaskView;
 /** tabBar蒙版 */
 @property (nonatomic, strong) UIView *tabBarMaskView;
+/** 左侧控制器 */
+@property (nonatomic, strong) UIViewController *leftVC;
 /** 是否已经打开侧滑 */
 @property (nonatomic, assign) BOOL hasOpen;
 @end
 
 @implementation UITabBarController (OKSliderApp)
 
+
+#pragma mark - ========== 左侧控制器 ==========
+
+- (void)setLeftVC:(UIViewController *)leftVC
+{
+    objc_setAssociatedObject(self, kLeftViewController, leftVC, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (UIViewController *)leftVC
+{
+    return objc_getAssociatedObject(self, kLeftViewController);
+}
 
 #pragma mark - ========== 左侧蒙版 ==========
 
@@ -128,10 +146,11 @@ static char const * const KHasOpen           = "KHasOpen";
 {
     UIViewController *leftVC = [[NSClassFromString(vcName) alloc] init];
     leftVC.edgesForExtendedLayout = UIRectEdgeNone;
-    leftVC.view.frame = CGRectMake(0, 0, Screen_Width, Screen_Height);
+    leftVC.view.frame = CGRectMake(kLeftVCStartX, 0, Screen_Width, Screen_Height);
     UIWindow *window = [UIApplication sharedApplication].delegate.window;
     [window insertSubview:leftVC.view atIndex:0];
     [self addChildViewController:leftVC];
+    self.leftVC = leftVC;
     
     self.leftMaskView = [[UIView alloc] init];
     self.leftMaskView.frame = CGRectMake(0, 0, Screen_Width, Screen_Height);
@@ -188,7 +207,7 @@ static char const * const KHasOpen           = "KHasOpen";
  */
 - (void)addScreenPan
 {
-    //右侧边缘滑动手势
+    //左侧边缘滑动手势
     UIScreenEdgePanGestureRecognizer *leftScreenPan = [[UIScreenEdgePanGestureRecognizer alloc] initWithTarget:self action:@selector(mainSlideHandlePan:)];
     leftScreenPan.edges = UIRectEdgeLeft;
     leftScreenPan.delegate = self;
@@ -216,38 +235,45 @@ static char const * const KHasOpen           = "KHasOpen";
 {
     CGPoint point = [gesture translationInView:self.view];
     
-    if (self.hasOpen) {
-        CGRect rect = self.view.frame;
-        rect.origin.x = MAX(MaxOffsetX + point.x, 0);
-        self.view.frame = rect;
+    //手势结束后超过这个值则修正位置
+    CGFloat convertX = 0.0;
+    CGRect selfRect = self.view.frame;
+    CGRect leftRect = self.leftVC.view.frame;
+    
+    if (self.hasOpen) { //将要关闭左侧视图
         
-        CGFloat percent = -point.x/(MaxOffsetX);
-        self.leftMaskView.alpha = percent * 0.5;
-        self.rightMaskView.alpha = 0.3 * (1-percent);
-        self.tabBarMaskView.alpha = self.rightMaskView.alpha;
+        //1.控制当前App视图位置
+        selfRect.origin.x = MIN(MAX(MaxOffsetX + point.x, 0), MaxOffsetX);
         
-        //手势结束后修正位置,超过约一半时向多出的一半偏移
-        if (gesture.state == UIGestureRecognizerStateEnded) {
-            BOOL open = (self.view.frame.origin.x > MaxOffsetX*0.85);
-            [self showAppSliderView:@(open)];
-        }
+        //2.控制当前左侧视图位置
+        leftRect.origin.x = MAX((selfRect.origin.x*0.27+kLeftVCStartX), kLeftVCStartX);
         
-    } else {
-        //屏幕宽度一半的百分比
-        CGRect rect = self.view.frame;
-        rect.origin.x = MIN((MAX(point.x, 0)), MaxOffsetX);
-        self.view.frame = rect;
+        //3.手势结束后，修正位置来关闭左侧视图
+        convertX = MaxOffsetX*0.85;
         
-        CGFloat percent = point.x/(self.view.frame.size.width/2);
-        self.leftMaskView.alpha = 0.5 * (1-percent);
-        self.rightMaskView.alpha = 0.3 * (percent-1);
-        self.tabBarMaskView.alpha = self.rightMaskView.alpha;
+    } else {//将要打开左侧视图
+        //1.控制当前App视图位置
+        selfRect.origin.x = MIN((MAX(point.x, 0)), MaxOffsetX);
         
-        //手势结束后修正位置,超过约一半时向多出的一半偏移
-        if (gesture.state == UIGestureRecognizerStateEnded) {
-            BOOL open = (self.view.frame.origin.x > self.view.frame.size.width/4);
-            [self showAppSliderView:@(open)];
-        }
+        //2.控制当前左侧视图位置
+        leftRect.origin.x = MIN((kLeftVCStartX + selfRect.origin.x*0.3), 0);
+        
+        //3.手势结束后，修正位置来打开左侧视图
+        convertX = self.view.frame.size.width/4;
+    }
+    
+    self.leftVC.view.frame = leftRect;
+    self.view.frame = selfRect;
+    
+    //控制两边蒙层的透明度
+    CGFloat percent = selfRect.origin.x/(MaxOffsetX);
+    self.leftMaskView.alpha = 0.5-percent*0.5;
+    self.rightMaskView.alpha = percent*0.3;
+    self.tabBarMaskView.alpha = self.rightMaskView.alpha;
+    
+    if (gesture.state == UIGestureRecognizerStateEnded) {
+        BOOL open = (self.view.frame.origin.x > convertX);
+        [self showAppSliderView:@(open)];
     }
 }
 
@@ -262,25 +288,29 @@ static char const * const KHasOpen           = "KHasOpen";
     
     BOOL open = [openFlag boolValue];
     
+    __block CGRect selfRect = self.view.frame;
+    __block CGRect leftRect = self.leftVC.view.frame;
+    
     [UIView animateWithDuration:0.3 animations:^{
         if (open) { //打开侧滑
-            CGRect rect = self.view.frame;
-            rect.origin.x = MaxOffsetX;
-            self.view.frame = rect;
+            selfRect.origin.x = MaxOffsetX;
+            leftRect.origin.x = 0;
             
             self.leftMaskView.alpha = 0.0;
             self.rightMaskView.alpha = 0.3;
-            self.tabBarMaskView.alpha = self.rightMaskView.alpha;
             
         } else { //关闭侧滑
-            CGRect rect = self.view.frame;
-            rect.origin.x = 0;
-            self.view.frame = rect;
+            selfRect.origin.x = 0;
+            leftRect.origin.x = kLeftVCStartX;
             
             self.leftMaskView.alpha = 0.5;
             self.rightMaskView.alpha = 0.0;
-            self.tabBarMaskView.alpha = self.rightMaskView.alpha;
         }
+        
+        self.view.frame = selfRect;
+        self.leftVC.view.frame = leftRect;
+        self.tabBarMaskView.alpha = self.rightMaskView.alpha;
+        
     } completion:^(BOOL finished) {
         self.hasOpen = open;
     }];
